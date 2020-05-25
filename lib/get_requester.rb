755653d5030412929d8336a0bf2cfc908c05a6ed
DAZUN XING <dazun.xing@gmail.com>
2020-05-25 18:45:58 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -3,6 +3,14 @@ require 'net/http'
 require 'open-uri'
 require 'json'
 class Getrequester
-  
+  attr_accessor :url
   def initialize(url)
+    @url =url
+  end
+  
+  def get_response_body
+    uri =URI.parse(@url)
+    result =Net::Http.get_response(uri)
+    result.body
+  end
 end
