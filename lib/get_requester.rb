25f63a8dd2094bfabc780ea076e445bbe9580ce7
DAZUN XING <dazun.xing@gmail.com>
2020-05-25 18:49:08 +0000

    Done.

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,20 @@
 # Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+class GetRequester
+  attr_accessor :url
+  def initialize(url)
+    @url =url
+  end
+  
+  def get_response_body
+    uri =URI.parse(@url)
+    result =Net::HTTP.get_response(uri)
+    result.body
+  end
+  
+  def parse_json
+    parsejson =JSON.parse(self.get_response_body)
+  end
+end
